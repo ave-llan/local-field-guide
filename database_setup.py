a947c577646683fc36e3dcd89787964af6638187
John Leszczynski <jrleszczynski@gmail.com>
2015-06-05 16:00:58 -0400

    remove unused imports

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,3 @@
-import os
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
