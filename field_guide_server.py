04ed4c11aa70ae4dbaf137b4766a53f8dbb1289e
John Leszczynski <jrleszczynski@gmail.com>
2015-06-07 09:01:38 -0400

    initial server for index

---
+++ field_guide_server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
+app = Flask(__name__)
+
+## import CRUD Operations ##
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Place, Species, SpeciesAtLocation
+
+# Create session and connect to DB
+engine = create_engine('sqlite:///fieldguide.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# home page
+@app.route('/')
+def homePage():
+    places = session.query(Place).all()
+    # last 10 species added to database
+    latestSpecies = session.query(Species).order_by(desc(Species.id)).all()[:10]
+    return render_template('index.html', places = places, latestSpecies = latestSpecies)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
