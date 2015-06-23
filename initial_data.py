c87b678e08872090d695f141b64598483a90d2d3
John Leszczynski <jrleszczynski@gmail.com>
2015-06-23 11:08:00 -0400

    remove newlines from initial data

--- initial_data.py
+++ initial_data.py
@@ -27,10 +27,7 @@ session.add(chimneySwift)
 session.commit()
 
 swiftsAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=chimneySwift.id,
-    tip = """
-    In the summer, almost always flying high above the park.  Some roost in the chimneys of 
-    the buildings along Adams Mill Road.
-    """)
+    tip = "In the summer, almost always flying high above the park.")
 
 session.add(swiftsAtWalter)
 session.commit()
@@ -46,9 +43,7 @@ session.add(cardinal)
 session.commit()
 
 cardinalAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=cardinal.id,
-    tip = """
-    Seen and heard year round.  Likes to perch near the top of trees.
-    """)
+    tip = "Seen and heard year round.  Likes to perch near the top of trees.")
 
 session.add(cardinalAtWalter)
 session.commit()
@@ -64,10 +59,7 @@ session.add(deer)
 session.commit()
 
 deerAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=deer.id,
-    tip = """
-    Freqently seen grazing or resting in the valley behind the basketball court.  Occasionally 
-    enters the park itself near the circle. 
-    """)
+    tip = "Freqently seen grazing or resting in the valley behind the basketball court.")
 
 session.add(deerAtWalter)
 session.commit()
@@ -83,9 +75,7 @@ session.add(redFox)
 session.commit()
 
 foxAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=redFox.id,
-    tip = """
-    Rarely seen in the valley behind the basketball court.  Look for motion under the vegetation. 
-    """)
+    tip = "Rarely seen in the valley behind the basketball court.  Look for motion under the vegetation.")
 
 session.add(foxAtWalter)
 session.commit()
@@ -101,9 +91,7 @@ session.add(ratSnake)
 session.commit()
 
 ratSnakeAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=ratSnake.id,
-    tip = """
-    In the summer of 2013, several rat snakes fell from the trees in the children's playground.
-    """)
+    tip = "In the summer of 2013, several large rat snakes fell from the trees above the children's playground.")
 
 session.add(ratSnakeAtWalter)
 session.commit()
@@ -119,9 +107,7 @@ session.add(tulipTree)
 session.commit()
 
 tulipAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=tulipTree.id,
-    tip = """
-    Medium tree near the drinking fountain, up above the basketball court. 
-    """)
+    tip = "Medium tree near the drinking fountain, up above the basketball court.")
 
 session.add(tulipAtWalter)
 session.commit()
@@ -137,10 +123,7 @@ session.add(rbWoodpecker)
 session.commit()
 
 rbWoodpeckerAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=rbWoodpecker.id,
-    tip = """
-    Easiest to find by listening for the distinctive 'churrr' call along with drumming. 
-    Often seen in the woods behind the dog park and in the trees on the other side of the valley.
-    """)
+    tip = "Often seen and heard in the woods behind the dog park and in the trees on the other side of the valley.")
 
 session.add(rbWoodpeckerAtWalter)
 session.commit()
@@ -156,9 +139,7 @@ session.commit()
 
 # add cardinal to dumbarton
 cardinalAtDumbarton = SpeciesOccurrence(place_id=dumbartonOaks.id, species_id=cardinal.id,
-    tip = """
-    Seen and heard year round throughout the park.
-    """)
+    tip = "Seen and heard year round throughout the park.")
 
 session.add(cardinalAtDumbarton)
 session.commit()
@@ -174,9 +155,7 @@ session.add(towhee)
 session.commit()
 
 towheeAtDumbarton = SpeciesOccurrence(place_id=dumbartonOaks.id, species_id=towhee.id,
-    tip = """
-    Often heard rummaging in the bushes up the hill that leads to the fenced Dumbarton Oaks Gardens.
-    """)
+    tip = "Often heard rummaging in the bushes up the hill that leads to the fenced Dumbarton Oaks Gardens.")
 
 session.add(towheeAtDumbarton)
 session.commit()
@@ -184,9 +163,7 @@ session.commit()
 
 # add deer to dumbarton
 deerAtDumbarton = SpeciesOccurrence(place_id=dumbartonOaks.id, species_id=deer.id,
-    tip = """
-    Often seen on the northwest side of the park in the woods around Observatory Circle and Whitehaven Street. 
-    """)
+    tip = "Often seen on the northwest side of the park in the woods around Observatory Circle and Whitehaven Street.")
 
 session.add(deerAtDumbarton)
 session.commit()
