375d93cc24a4a16674bbb479617a83f910a1a082
John Leszczynski <jrleszczynski@gmail.com>
2015-06-29 17:18:52 -0400

    fill species description with AJAX call from wikipedia

--- static/fillSpeciesCards.js
+++ static/fillSpeciesCards.js
@@ -1,6 +1,24 @@
 var $cards = $('.speciesCard');
 $cards.each(function(card) {
     var sciName = $(this).find('#scientific-name').text();
-    $(this).find('#species-description').text(card);
-    console.log(sciName);
+    var description = $(this).find('#species-description');
+
+    // load wikipedia description
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?' +
+                  'format=json&action=query&prop=extracts&exintro=' + 
+                  '&explaintext=&redirects=&exsectionformat=plain&exsentences=2' +
+                  '&titles=' + sciName;
+    $.ajax({
+        url: wikiUrl,
+        dataType: 'jsonp',
+        success: function(data) {
+            console.log(data);
+            var key = Object.keys(data.query.pages)[0];
+            var url = 'http://en.wikipedia.org/wiki/' + data.query.pages[key].title;
+            description.text(
+                data.query.pages[key].extract 
+                );
+            description.append(' <a href="' + url + '">Wikipedia</a>');
+        }
+    });
 })
