338c782041d44d106d9e5d0c401203bce66725e4
John Leszczynski <jrleszczynski@gmail.com>
2015-06-30 09:28:16 -0400

    put card pictures in a figure

--- static/fillSpeciesCards.js
+++ static/fillSpeciesCards.js
@@ -2,7 +2,6 @@
 var $cards = $('.speciesCard');
 $cards.each(function() {
     var card = $(this);
-    console.log(card);
     var sciName = $(this).find('#scientific-name').text();
     var description = $(this).find('#species-description');
 
@@ -31,19 +30,22 @@ $cards.each(function() {
                     'method=flickr.photos.search&api_key=6417b277d897c7a6575bd941770bd1e5' +
                     '&sort=relevance&per_page=1&format=json&nojsoncallback=1' +
                     '&text=' + sciName;
-    console.log(flickrUrl);
     $.ajax({
         url: flickrUrl,
         dataType: 'json',
         success: function(data) {
             var photo = data.photos.photo[0];
-
+            console.log(photo);
             var photoUrl = 'https://farm' + photo.farm + 
                            '.staticflickr.com/' + photo.server +
                            '/' + photo.id + '_' + photo.secret +
                            '_z.jpg';
             console.log(photoUrl);
-            card.prepend('<img src="' + photoUrl + '">');
+            card.prepend(
+                '<figure>' +
+                '<img src="' + photoUrl + '">' +
+                '</figure>'
+                );
         }
     })
 })
