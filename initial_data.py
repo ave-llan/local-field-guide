274992814d5644f571d2fe94636d7f54b0939b7a
John Leszczynski <jrleszczynski@gmail.com>
2015-06-30 17:39:13 -0400

    revamp initial data to read from json files and create field guides

--- initial_data.py
+++ initial_data.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Base, Place, Species, SpeciesOccurrence, engine
+from database_setup import Base, User, Place, Species, SpeciesOccurrence, engine
+import json
+from os import listdir
+
 
 Base.metadata.bind = engine
 
@@ -9,154 +12,54 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# Create Walter Pierce Park instance
-walterPierce = Place(name="Walter Pierce Park", 
-    longitude="-77.046051", latitude="38.924066")
 
-session.add(walterPierce)
+# takes a json file, creates a place and adds species
+def fieldGuideFromJSON(filename, owner):
+    # read file
+    with open(filename) as guide_file:
+        guide_data = json.loads(guide_file.read())
+
+    # Create field guide
+    fieldguide = Place(
+        name=guide_data['name'],
+        latitude=guide_data['latitude'],
+        longitude=guide_data['longitude'],
+        user_id=owner.id)
+    session.add(fieldguide)
+    session.commit()
+
+    for s in guide_data['species']:
+        # try to get species from database (it might not be there yet)
+        sp = session.query(Species).filter_by(
+            scientific_name=s['scientific_name']).scalar()
+        # if species not in database, first add it
+        if sp is None:
+            sp = Species(
+                common_name=s['common_name'],
+                scientific_name=s['scientific_name'],
+                category=s['type'])
+            session.add(sp)
+            session.commit()
+        # add species to this field guide
+        occurrance = SpeciesOccurrence(
+            place_id=fieldguide.id,
+            species_id=sp.id,
+            tip=s['tip'])
+        session.add(occurrance)
+        session.commit()
+
+
+# Create inital user to whom all initial field guides will be assigned
+rudy = User(
+    name="Rudy",
+    email="stinkypaw@gmail.com",
+    picture="http://c2.staticflickr.com/4/3909/15252090882_19df37d630_n.jpg")
+session.add(rudy)
 session.commit()
 
+# add all files in initial-field-guides directory to database
+for fgJSON in listdir('initial-field-guides'):
+    filepath = 'initial-field-guides/' + fgJSON
+    fieldGuideFromJSON(filepath, rudy)
 
-# add Chimnney Swift
-chimneySwift = Species(common_name="Chimney Swift",
-    scientific_name="Chaetura pelagica", 
-    category="bird")
-
-session.add(chimneySwift)
-session.commit()
-
-swiftsAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=chimneySwift.id,
-    tip = "In the summer, almost always flying high above the park.")
-
-session.add(swiftsAtWalter)
-session.commit()
-
-
-# add Northern Cardinal
-cardinal = Species(common_name="Northern Cardinal",
-    scientific_name="Cardinalis cardinalis", 
-    category="bird")
-
-session.add(cardinal)
-session.commit()
-
-cardinalAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=cardinal.id,
-    tip = "Seen and heard year round.  Likes to perch near the top of trees.")
-
-session.add(cardinalAtWalter)
-session.commit()
-
-
-# add deer
-deer = Species(common_name="White-Tailed Deer",
-    scientific_name="Odocoileus virginianus", 
-    category="mammal")
-
-session.add(deer)
-session.commit()
-
-deerAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=deer.id,
-    tip = "Freqently seen grazing or resting in the valley behind the basketball court.")
-
-session.add(deerAtWalter)
-session.commit()
-
-
-# add red fox
-redFox = Species(common_name="American Red Fox",
-    scientific_name="Vulpes vulpes fulvus", 
-    category="mammal")
-
-session.add(redFox)
-session.commit()
-
-foxAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=redFox.id,
-    tip = "Rarely seen in the valley behind the basketball court.  Look for motion under the vegetation.")
-
-session.add(foxAtWalter)
-session.commit()
-
-
-# add rat snake
-ratSnake = Species(common_name="Black Rat Snake",
-    scientific_name="Elaphe obsoleta", 
-    category="reptile")
-
-session.add(ratSnake)
-session.commit()
-
-ratSnakeAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=ratSnake.id,
-    tip = "In the summer of 2013, several large rat snakes fell from the trees above the children's playground.")
-
-session.add(ratSnakeAtWalter)
-session.commit()
-
-
-# add tulip tree
-tulipTree = Species(common_name="Tuliptree",
-    scientific_name="Liriodendron tulipifera", 
-    category="tree")
-
-session.add(tulipTree)
-session.commit()
-
-tulipAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=tulipTree.id,
-    tip = "Medium tree near the drinking fountain, up above the basketball court.")
-
-session.add(tulipAtWalter)
-session.commit()
-
-
-# add Red-bellied woodpecker
-rbWoodpecker = Species(common_name="Red-bellied woodpecker",
-    scientific_name="Melanerpes carolinus", 
-    category="bird")
-
-session.add(rbWoodpecker)
-session.commit()
-
-rbWoodpeckerAtWalter = SpeciesOccurrence(place_id=walterPierce.id, species_id=rbWoodpecker.id,
-    tip = "Often seen and heard in the woods behind the dog park and in the trees on the other side of the valley.")
-
-session.add(rbWoodpeckerAtWalter)
-session.commit()
-
-
-#####################################
-# Create Dumbarton Oaks instance
-dumbartonOaks = Place(name="Dumbarton Oaks Park", 
-    longitude="-77.064373", latitude="38.916790")
-
-session.add(dumbartonOaks)
-session.commit()
-
-# add cardinal to dumbarton
-cardinalAtDumbarton = SpeciesOccurrence(place_id=dumbartonOaks.id, species_id=cardinal.id,
-    tip = "Seen and heard year round throughout the park.")
-
-session.add(cardinalAtDumbarton)
-session.commit()
-
-
-# add Eastern Towhee
-towhee = Species(common_name="Eastern Towhee",
-    scientific_name="Pipilo erythrophthalmus", 
-    category="bird")
-
-session.add(towhee)
-session.commit()
-
-towheeAtDumbarton = SpeciesOccurrence(place_id=dumbartonOaks.id, species_id=towhee.id,
-    tip = "Often heard rummaging in the bushes up the hill that leads to the fenced Dumbarton Oaks Gardens.")
-
-session.add(towheeAtDumbarton)
-session.commit()
-
-
-# add deer to dumbarton
-deerAtDumbarton = SpeciesOccurrence(place_id=dumbartonOaks.id, species_id=deer.id,
-    tip = "Often seen on the northwest side of the park in the woods around Observatory Circle and Whitehaven Street.")
-
-session.add(deerAtDumbarton)
-session.commit()
 
