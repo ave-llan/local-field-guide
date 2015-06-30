8db18fd7efea61a74d66bb8be894a490f88d866b
John Leszczynski <jrleszczynski@gmail.com>
2015-06-30 11:45:59 -0400

    render login page

--- field_guide_server.py
+++ field_guide_server.py
@@ -23,7 +23,7 @@ def showLogin():
     # 32 charachter state key
     state=''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" %login_session['state']
+    return render_template('login.html')
 
 
 
