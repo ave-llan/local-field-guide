a4833af9c466338996579ee58381f5e6d09d0924
John Leszczynski <jrleszczynski@gmail.com>
2015-07-01 09:30:59 -0400

    add first and last names to User table

--- database_setup.py
+++ database_setup.py
@@ -13,6 +13,8 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+    given_name = Column(String(250))
+    family_name = Column(String(250))
     picture = Column(String(250))
 
 
