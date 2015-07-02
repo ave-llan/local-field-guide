56e418c768d184b5df420520c2a32e2bf3ed1618
John Leszczynski <jrleszczynski@gmail.com>
2015-07-02 09:49:54 -0400

    set view of maps to proper location

--- static/createMaps.js
+++ static/createMaps.js
@@ -1,14 +1,13 @@
-// Provide your access token
 L.mapbox.accessToken = 'pk.eyJ1IjoiZGlzdHJpY3RpbnJvYWRzIiwiYSI6Ik5pX2NMd2MifQ.VAzyiIlIyhmMduGNEUeGHw';
 
-
+// select all the maps
 $maps = $( ".field-guide-map" )
 
+// create each map and set its view to the correct position
 $maps.each(function(index) {
     var mapName = 'map' + String(index);
-    console.log(mapName);
-    L.mapbox.map(mapName, 'districtinroads.mjpi6m96');
+    var lat = $maps[index].getAttribute("latitude")
+    var lng = $maps[index].getAttribute("longitude")
+    var map = L.mapbox.map(mapName, 'districtinroads.mjpi6m96');
+    map.setView([lat, lng], 14)
 })
-
-
-
