95187f8c9dcd2ac82383d432b94fd7a2f1488d49
John Leszczynski <jrleszczynski@gmail.com>
2015-07-02 08:04:26 -0400

    change to 0 based indexing for map ids

--- static/createMaps.js
+++ static/createMaps.js
@@ -5,8 +5,7 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiZGlzdHJpY3RpbnJvYWRzIiwiYSI6Ik5pX2NMd2MifQ.VA
 $maps = $( ".field-guide-map" )
 
 $maps.each(function(index) {
-    console.log(index + 1);
-    var mapName = 'map' + String(index+1);
+    var mapName = 'map' + String(index);
     console.log(mapName);
     L.mapbox.map(mapName, 'districtinroads.mjpi6m96');
 })
