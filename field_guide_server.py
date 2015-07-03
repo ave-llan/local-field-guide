23b0fb6f5d4ac17c02b1888ec75e64ed58dbbe45
John Leszczynski <jrleszczynski@gmail.com>
2015-07-03 19:41:20 -0400

    update facebook login

--- field_guide_server.py
+++ field_guide_server.py
@@ -362,17 +362,9 @@ def fbconnect():
         user_id = createUser(login_session)
     login_session['user_id'] = user_id
 
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-
-    flash("Now logged in as %s" % login_session['username'])
-    return output
+    flash("Welcome, {}! You are now logged in.".format(login_session['given_name']))
+    print "facebook login successful!"
+    return 'Login successful! Redirecting...'
 
 
 # Revokes a current Google user's token and resets their login_session.
